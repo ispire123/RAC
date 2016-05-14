80e2ae56c7793f84429aa453d080e640be17405e
Imran Oztechie <imran.oztechie@gmail.com>
2016-05-14 11:50:08 +1000

    Initial Commit

---
+++ RAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAC
+//
+//  Created by admin on 14/05/2016.
+//  Copyright © 2016 Rent A Car. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
